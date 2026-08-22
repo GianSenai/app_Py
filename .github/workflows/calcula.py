5a600958dd57e48d8a371ab776be528ab7af6325
Gian Miguel Oliveira <gian.migueloliveiraborges@gmail.com>
2026-08-22 11:58:11 -0300

    vambora

---
+++ .github/workflows/calcula.py
@@ -0,0 +1,9 @@
+def calcular(a, b):
+    return a + b
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+resultado = calcular(numero1, numero2)
+
+print("Resultado:", resultado)
